7bc20e407bf56f50931364753262205322e96da8
nvidia <justincosta10@gmail.com>
2025-12-18 15:14:07 -0700

    found camera id

--- so101-ik/capture_image.py
+++ so101-ik/capture_image.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 # Create camera configuration
 config = RealSenseCameraConfig(
-    serial_number_or_name="YOUR_SERIAL_NUMBER", color_mode=ColorMode.RGB, width=640, height=480, fps=30
+    serial_number_or_name="video2", color_mode=ColorMode.RGB, width=640, height=480, fps=30
 )
 
 # Connect to camera
