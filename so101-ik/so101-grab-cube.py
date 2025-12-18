e2855f802bc9d30241c44d04f34a0d86bc9ddb39
nvidia <justincosta10@gmail.com>
2025-12-18 14:29:17 -0700

    added degree movement

--- so101-ik/so101-grab-cube.py
+++ so101-ik/so101-grab-cube.py
@@ -11,7 +11,7 @@ action = {
     "shoulder_lift.pos": 30.0,
     "elbow_flex.pos": -20.0,
     "wrist_flex.pos": 10.0,
-    "wrist_roll.pos": 0.0,
+    "wrist_roll.pos": 30.0,
     "gripper.pos": 50.0,  # 0=closed, 100=open
 }
 robot.send_action(action)
