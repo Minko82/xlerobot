8f079fe1ab0f897ae0182f2ae51303a7acc4c938
Justin Costa <justincosta10@gmail.com>
2025-12-18 17:25:24 -0700

    rolling back

--- so101-ik/so101-cube-vision.py
+++ so101-ik/so101-cube-vision.py
@@ -3,15 +3,9 @@ from pathlib import Path
 
 script_dir = Path(__file__).resolve().parent
 
-color_raw = o3d.io.read_image(
-    str(script_dir / "images" / "opencv__dev_video4.png")
-)
-depth_raw = o3d.io.read_image(
-    str(script_dir / "images" / "opencv__dev_video2.png")
-)
+color_raw = o3d.io.read_image(str(script_dir / "images" / "opencv__dev_video4.png"))
+depth_raw = o3d.io.read_image(str(script_dir / "images" / "opencv__dev_video2.png"))
 
 rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
-    color_raw,
-    depth_raw,
-    convert_rgb_to_intensity=False
+    color_raw, depth_raw, convert_rgb_to_intensity=False
 )
